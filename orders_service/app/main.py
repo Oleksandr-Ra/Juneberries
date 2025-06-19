2d2cbc736b89a805eeeec7fc0257d09bac48b072
Oleksandr Radchenko <oradchenko108@gmail.com>
2025-06-19 10:31:51 +0530

    change description

--- orders_service/app/main.py
+++ orders_service/app/main.py
@@ -7,6 +7,6 @@ from api_v1 import router as api_v1_router
 app = FastAPI(
     default_response_class=ORJSONResponse,
     title='Order Service',
-    description='Список оформленных заказов. FastAPI, PostgreSQL, SQLAlchemy(v2), Docker, Pytest',
+    description='Список оформленных заказов. FastAPI, PostgreSQL, SQLAlchemy(v2), Kafka, Docker',
 )
 app.include_router(api_v1_router)
