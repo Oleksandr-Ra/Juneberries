26721851c5d7eaf6652fce799a6ed9ee410c5a68
Oleksandr Radchenko <oradchenko108@gmail.com>
2025-06-23 17:47:01 +0530

    logic for categories

--- catalog_service/app/api_v1/categories/views.py
+++ catalog_service/app/api_v1/categories/views.py
@@ -1,8 +1,33 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status
 
-router = APIRouter(tags=['Categories'])
+from models import Category
+from . import services
+from .schemas import CategorySchema, CategoriesSchema
+
+router = APIRouter(prefix='/categories', tags=['Categories'])
 
 
-@router.get('/categories',)
-async def get_categories():
+@router.post('', response_model=CategorySchema, status_code=status.HTTP_201_CREATED)
+async def create_category(
+        category: Category = Depends(services.create_category)
+):
+    return category
+
+
+@router.get('', response_model=list[CategoriesSchema])
+async def get_categories(
+        categories: list[Category] = Depends(services.get_categories)
+):
+    return categories
+
+
+@router.put('/{id}', response_model=CategorySchema)
+async def update_category(
+        category: Category = Depends(services.update_category)
+):
+    return category
+
+
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT, dependencies=[Depends(services.delete_category)])
+async def delete_category():
     pass
