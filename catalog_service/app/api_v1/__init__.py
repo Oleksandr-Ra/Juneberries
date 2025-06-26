4229cd3ee5eebc11b9139167023ba4d0158be57a
Oleksandr Radchenko <oradchenko108@gmail.com>
2025-06-26 22:02:08 +0530

    rename views into routs

--- catalog_service/app/api_v1/__init__.py
+++ catalog_service/app/api_v1/__init__.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
 from config import settings
-from .categories.views import router as categories_router
-from .products.views import router as products_router
+from .categories.routes import router as categories_router
+from .products.routes import router as products_router
 
 router = APIRouter(prefix=settings.api_v1_prefix)
 router.include_router(categories_router)
