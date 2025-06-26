a8fa954d8c638903386715bca09efa38071b9be3
Oleksandr Radchenko <oradchenko108@gmail.com>
2025-06-26 22:13:43 +0530

    rename views into routes

--- orders_service/app/api_v1/__init__.py
+++ orders_service/app/api_v1/__init__.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from config import settings
-from .orders.views import router as orders_router
+from .orders.routes import router as orders_router
 
 router = APIRouter(prefix=settings.api_v1_prefix)
 router.include_router(orders_router)
