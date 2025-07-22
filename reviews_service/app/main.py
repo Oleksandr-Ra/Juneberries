689c9d6c804b112f5414c74ac5ff37288f416758
Oleksandr Radchenko <oradchenko108@gmail.com>
2025-07-22 17:25:35 +0530

    fix db connection

--- reviews_service/app/main.py
+++ reviews_service/app/main.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, Request
 from fastapi.responses import ORJSONResponse, JSONResponse
-from motor.motor_asyncio import AsyncIOMotorClient
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 
 from api_v1 import router as api_v1_router
 from config import settings
@@ -15,15 +15,13 @@ logger = setup_logger('reviews_service')
 @asynccontextmanager
 async def lifespan(app_: FastAPI):
     client = AsyncIOMotorClient(settings.db.url)
-    app_.state.mongo_client = client
     app_.state.db = client[settings.db.reviews_mongo_db]
     logger.info('Connected to MongoDB')
 
     yield
 
-    if app_.state.mongo_client:
-        app_.state.mongo_client.close()
-        logger.info('MongoDB connection closed')
+    client.close()
+    logger.info('MongoDB connection closed')
 
 
 app = FastAPI(
@@ -49,8 +47,8 @@ def liveness_check():
 @app.get('/ready', tags=['HealthCheck'])
 async def readiness_check(request: Request):
     try:
-        mongo_client: AsyncIOMotorClient = request.app.state.mongo_client
-        await mongo_client.admin.command('ping')
+        db: AsyncIOMotorDatabase = request.app.state.db
+        await db.command('ping')
 
         return {'status': 'ready'}
 
